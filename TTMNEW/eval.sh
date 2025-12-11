8f435a26ce46d2cc81062b5ef2a1e7f22abc56cf
SHAREN111 <2534676560@qq.com>
2025-12-11 16:20:49 +0800

    描述你的修改内容

--- TTMNEW/eval.sh
+++ TTMNEW/eval.sh
@@ -1,3 +1,6 @@
 
 CUDA_VISIBLE_DEVICES=3 python3 ./evaluation/vbench_metric.py --exp_name sparsevar_0.9           &
+
+
+
 CUDA_VISIBLE_DEVICES=4 python3 ./evaluation/vbench_metric.py --exp_name sparsevar_0.95          &
